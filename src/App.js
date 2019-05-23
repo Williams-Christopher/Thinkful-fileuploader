7b883d34407c445ada17ba4a03ff1552136d607c
Christopher Williams <cw@chriswillia.ms>
2019-05-23 10:10:30 -0500

    Implements callback props for searching.

--- src/App.js
+++ src/App.js
@@ -12,10 +12,27 @@ class App extends Component {
     };
   }
 
+  updateSearchTerm(term) {
+    this.setState({
+      searchTerm: term,
+    })
+  }
+
+  updateFilterOption(option) {
+    this.setState({
+      filterOption: option,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar searchTerm={this.state.searchTerm} filterOption={this.state.filterOption} />
+        <SearchBar
+          searchTerm={this.state.searchTerm}
+          filterOption={this.state.filterOption}
+          handleUpdate={term => this.updateSearchTerm(term)}
+          handleFilterChange={option => this.updateFilterOption(option)}
+        />
         <FilterableList files={this.props.files} searchTerm={this.state.searchTerm} filterOption={this.state.filterOption} />
       </div>
     );
